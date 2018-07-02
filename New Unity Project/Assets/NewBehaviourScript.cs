8ba37bebf1d2d3bba212427886402df7006add22
bahadir <bahadirihsanbabacan@gmail.com>
2018-07-02 18:08:10 +0300

    yenii

---
+++ New Unity Project/Assets/NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    
+
+    static void degertipi(out int x)
+    {
+        x = 110;
+    }
+    
+
+	// Use this for initialization
+	void Start () {
+
+
+        int x ;
+        degertipi(out x);
+        Debug.Log(x);
+		
+	}
+	
+	
+}
